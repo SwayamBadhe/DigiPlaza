518ecb683d522d1b3d409dc9b02802ccda32a316
Swayam Badhe <swayampbadhe@gmail.com>
2024-02-20 15:40:19 +0530

    refactor: reorganize router structure with introduction of authRouter

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,13 +1,12 @@
-import { publicProcedure, router } from './trpc';
+import { authRouter } from './auth-router';
+import { router } from './trpc';
 
 /**
  * a public procedure refers to a procedure that is accessible and callable from the client-side code.
  * Procedures are functions or methods defined on the server that the client can invoke remotely.
  */
 export const appRouter = router({
-  anyApiRoute: publicProcedure.query(() => {
-    return 'hello';
-  }),
+  auth: authRouter,
 });
 
 /**
