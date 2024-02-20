8e0057a3997a735ec43ff88f65e7d47abae16cf7
Swayam Badhe <swayampbadhe@gmail.com>
2024-02-20 20:25:11 +0530

    feat: implement authentication procedures in authRouter

---
+++ src/trpc/auth-router.ts
@@ -0,0 +1,61 @@
+import { TRPCError } from '@trpc/server';
+import { getPayLoadClient } from '../get-payload';
+import { AuthCredentialsValidator } from '../lib/validators/account-credentails';
+import { publicProcedure, router } from './trpc';
+import { z } from 'zod';
+
+export const authRouter = router({
+  createPayloadUser: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input }) => {
+      const { email, password } = input;
+      const payload = await getPayLoadClient();
+
+      /**
+       * check if user already exists
+       */
+      const { docs: users } = await payload.find({
+        collection: 'users',
+        where: {
+          email: {
+            equals: email,
+          },
+        },
+      });
+
+      if (users.length !== 0)
+        throw new TRPCError({
+          code: 'CONFLICT',
+          message: 'User already exists',
+        });
+
+      await payload.create({
+        collection: 'users',
+        data: {
+          email,
+          password,
+          role: 'user',
+        },
+      });
+
+      return { success: true, sentToEmail: email };
+    }),
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => {
+      const { token } = input;
+
+      const payload = await getPayLoadClient();
+
+      const isVerified = await payload.verifyEmail({
+        collection: 'users',
+        token,
+      });
+
+      if (!isVerified)
+        throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Invalid token' });
+
+      return { suceess: true };
+    }),
+});
