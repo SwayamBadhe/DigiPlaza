0a2c861341694aa62c8ef1d12f568770a36764f3
Swayam Badhe <swayampbadhe@gmail.com>
2024-02-22 14:44:37 +0530

    feat: Add error messages for form validation in Sign-Up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -91,6 +91,11 @@ const Page = () => {
                     })}
                     placeholder="you@example.com"
                   />
+                  {errors?.email && (
+                    <p className="text-sm text-red-500">
+                      {errors.email.message}
+                    </p>
+                  )}
                 </div>
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
@@ -102,6 +107,11 @@ const Page = () => {
                     })}
                     placeholder="Password"
                   />
+                  {errors?.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
+                  )}
                 </div>
                 <Button>Sign Up</Button>
               </div>
